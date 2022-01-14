e4f069aa4c72682afb6566ee0d24aa0b5530de81
VedaNadhaReddy <97672195+VedaNadhaReddy@users.noreply.github.com>
2022-01-14 17:13:07 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,58 @@
+//method demo
+
+package Java_assignments;
+public class methodExecution {
+	public int multipynumbers(int a,int b) {
+		int z=a*b;
+		return z;
+	}
+	public static void main(String[] args) {
+
+		methodExecution b=new methodExecution();
+		int ans= b.multipynumbers(10,3);
+		System.out.println("Multipilcation is :"+ans);
+		}
+}
+
+//call by value
+
+package Java_assignments;
+	public class callMethod {
+
+	int val=150;
+
+	int operation(int val) {
+		val =val*10/100;
+		return(val);
+	}
+
+	public static void main(String args[]) {
+		callMethod d = new callMethod();
+		System.out.println("Before operation value of data is "+d.val);
+		d.operation(100);
+		System.out.println("After operation value of data is "+d.val);
+		}
+	}
+
+//method overloading
+
+package Java_assignments;
+public class OverloadMethod {
+		
+	public void area(int b,int h)
+	    {
+	         System.out.println("Area of Triangle : "+(0.5*b*h));
+	    }
+	    public void area(int r) 
+	    {
+	         System.out.println("Area of Circle : "+(3.14*r*r));
+	    }
+
+	    public static void main(String args[])
+	   {
+
+	OverloadMethod ob=new OverloadMethod();
+	       ob.area(10,12);
+	       ob.area(5);  
+	   }
+	}
